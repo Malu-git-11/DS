a005f4b71263b27c970d425749f29b1767e17be3
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-09-23 17:55:27 -0300

    Add files via upload

---
+++ SistemadeGestãodeBibliotecaConstructor/Usuario.java
@@ -0,0 +1,30 @@
+package SistemadeGestãodeBibliotecaConstructor;
+public class Usuario {
+
+    public Usuario(String nome, String tipoUsuario, String data) {
+        this.nome = nome;
+        this.tipoUsuario = tipoUsuario;
+        this.data = data;
+    }
+
+    String nome;
+    String tipoUsuario;
+    String data;
+    
+    public void calcularPrazoDevolucao(){
+        if(tipoUsuario.equalsIgnoreCase("professor")){
+            System.out.println("Voce devera devolver o livro em : " + "30 dias"); 
+         }else if(tipoUsuario.equalsIgnoreCase("aluno")){
+            System.out.println("Voce devera devolver o livro em : " + "15 dias"); 
+        }else if(tipoUsuario.equalsIgnoreCase("visitante")){
+            System.out.println("Voce devera devolver o livro em : 7 dias");
+        }
+    }
+    public void exibirInfo(){
+            System.out.println("--- Gestao de Livros ---");
+            System.out.println("Nome do Livro: " + nome);
+            System.out.println("Tipo de Usuario: " + tipoUsuario);
+            System.out.println("Dia que pegou o livro: " + data);
+            System.out.println("------------------------");
+    }
+}
